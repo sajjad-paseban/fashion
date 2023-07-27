76618929a6796844ec021534fa798d35c0c6aa4a
iamsajad <s.pr98@yahoo.com>
2023-07-27 17:18:45 +0330

    tinymce package is added and some styles of datatable package are changed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,6 +14,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+    @stack('style')
+
 </head>
 <body>
     <section class="content">
@@ -30,7 +32,7 @@
     <script src="{{asset('dist/js/jquery.validate.js')}}"></script>
     <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
-    <script src="{{asset('admin/js/app.js')}}"></script>
+    <script type="module" src="{{asset('admin/js/app.js')}}"></script>
     @stack('script')
 </body>
 </html>
--- resources/views/admin/pages/posts/index.blade.php
+++ resources/views/admin/pages/posts/index.blade.php
@@ -1,4 +1,66 @@
+@php
+    $buttons = [
+        ['name'=>'پست جدید','href'=>route('admin.post.create')]
+    ];
+@endphp
 @extends('admin.index')
 @section('content')
-    as
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h4 class="title p-4">
+                    مدیریت پست ها
+                </h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <x-box title="پست ها" :buttons="$buttons">
+                    <table id="mytable" class="table table-hover text-center">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>عنوان</th>
+                                <th>دسته بندی</th>
+                                <th>وضعیت</th>
+                                <th>عملیات</th>
+                                <th>تاریخ ثیت</th>
+                                <th>تاریخ آخرین ویرایش</th>
+                                <th>کاربر ثبت کننده</th>
+                                <th>کاربر ویرایش کننده</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <button class="btn btn-sm bg-light-danger">
+                                        حذف
+                                    </button>
+                                    <a href="" class="btn btn-sm bg-light-purple">
+                                        ویرایش
+                                    </a>
+                                </td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </x-box>
+            </div>
+        </div>
+    </div>
+
+    @push('script')
+        <script>
+            new DataTable('#mytable');
+            document.querySelector('.dataTables_filter>label>input').placeholder = 'جستجو...';
+        </script>
+    @endpush
 @endsection
